1909901d4720cdcf26c7d2d4f2f1f58dd3931921
gmccannon <gwm13@uakron.edu>
2024-09-22 16:29:53 -0400

    Add individual page content

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TopBar from './Components/TopBar';
+
+import TitlePage from './Pages/TitlePage';
 import BooksPage from './Pages/BooksPage';
 import TVPage from './Pages/TVPage';
 import MoviesPage from './Pages/MoviesPage';
-import TitlePage from './Pages/TitlePage';
 import GamesPage from './Pages/GamesPage';
+
+import BooksReviewPage from './Pages/BooksReviewpage'; 
+import MoviesReviewPage from './Pages/MoviesReviewPage';
+import GamesReviewPage from './Pages/GamesReviewPage';
+import TVShowsReviewPage from './Pages/TVShowReviewPage';
 
 function App() {
   return (
@@ -13,10 +19,16 @@ function App() {
         <TopBar />
         <Routes>
             <Route path="/" element={<TitlePage />}/>
+
             <Route path="/Movies" element={<MoviesPage />}/>
             <Route path="/TVShows" element={<TVPage />}/>
             <Route path="/Books" element={<BooksPage />}/>
             <Route path="/Games" element={<GamesPage />}/>
+
+            <Route path="/Movies/:id" element={<MoviesReviewPage />}/>
+            <Route path="/TVShows/:id" element={<TVShowsReviewPage />}/>
+            <Route path="/Books/:id" element={<BooksReviewPage />}/>
+            <Route path="/Games/:id" element={<GamesReviewPage />}/>
         </Routes>
     </BrowserRouter>
     );
--- front/src/Pages/TitlePage.tsx
+++ front/src/Pages/TitlePage.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import Header from '../Components/Header';
-import BookCard from '../Components/BookCard';
 
 const GridContainer = styled.div`
     display: flex;
